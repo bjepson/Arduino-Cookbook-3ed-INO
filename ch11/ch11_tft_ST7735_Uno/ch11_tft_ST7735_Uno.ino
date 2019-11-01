a1392db6f68b1d5d71f41460f2b10e9823e1054e
Brian Jepson <bjepson@gmail.com>
2019-10-31 23:24:30 -0400

    new file

---
+++ ch11/ch11_tft_ST7735_Uno/ch11_tft_ST7735_Uno.ino
@@ -0,0 +1,64 @@
+/*
+ * Adafruit GFX ST7735 sketch
+ * Display text and a moving ball on the display
+ */
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
+// Define the connections for your panel. This will vary depending on
+// your display and the board you are using
+#define TFT_CS   10
+#define TFT_RST  9 
+#define TFT_DC   8
+#define TFT_BACKLIGHT  7
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+void setup(void) 
+{
+  tft.initR(INITR_BLACKTAB); // Init ST7735R chip 1.8" 128x160 LCD
+  
+  pinMode(TFT_BACKLIGHT, OUTPUT); // Backlight pin
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Turn on the backlight
+  
+  tft.setRotation(2); // This will depend on how you mounted the panel
+
+  tft.fillScreen(ST77XX_BLACK); // Fill the screen with black
+
+  // Display some text in a variety of fonts
+  tft.setCursor(0, 0);
+  tft.setTextWrap(false);
+
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Small");
+
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(2);
+  tft.println("Medium");
+
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(3);
+  tft.println("Large");
+}
+
+int ballDir = 1;          // Current direction of motion
+int ballDiameter = 8;     // Diameter
+int ballX = ballDiameter; // Starting X position
+void loop() 
+{
+  // If the ball is approaching the edge of the screen, reverse direction
+  if (ballX >= tft.width() - ballDiameter || ballX < ballDiameter) {
+    ballDir *= -1;
+  }
+
+  ballX += ballDir; // Move the ball's X position
+
+  // Calculate the Y position based on where the cursor was
+  int ballY = tft.getCursorY() + ballDiameter*2;
+
+  tft.fillCircle(ballX, ballY, ballDiameter/2, 0xffff00); // Yellow ball
+  delay(25);
+  tft.fillCircle(ballX, ballY, ballDiameter/2, 0x000000); // Erase the ball
+}
