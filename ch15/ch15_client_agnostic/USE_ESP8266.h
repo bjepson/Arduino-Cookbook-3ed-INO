ded529fbc263ac46fc8010247a2b439319fee2b3
Brian Jepson <bjepson@gmail.com>
2019-11-23 22:42:00 -0500

    missing

---
+++ ch15/ch15_client_agnostic/USE_ESP8266.h
@@ -0,0 +1,16 @@
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+const char ssid[] = "YOUR SSID";
+const char password[] = "YOUR PASSWORD";
+WiFiClient client;
+bool configureNetwork()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) // Wait for connection
+  {
+    delay(500);
+    Serial.print("Waiting for connection to "); Serial.println(ssid);
+  }
+  return true;
+}
