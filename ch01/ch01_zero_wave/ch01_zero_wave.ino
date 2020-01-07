6630d07d806e227e56a533d909a4cf9d08cf8321
Brian Jepson <bjepson@gmail.com>
2020-01-06 22:54:53 -0500

    fixed that pesky sine wave issue

--- ch01/ch01_zero_wave/ch01_zero_wave.ino
+++ ch01/ch01_zero_wave/ch01_zero_wave.ino
@@ -1,5 +1,8 @@
-const int outputPin = A0;    // headphones connected to analog 0
-const int sensorPin = A1;    // connect sensor to analog input 1
+/*
+ * Zero wave sketch
+ */
+const int outputPin = A0; // headphones connected to analog 0
+const int sensorPin = A1; // connect sensor to analog input 1
 
 const int low  = 200;
 const int high = 800;
@@ -11,22 +14,21 @@ const int maxDur = 1000000/(sampleCount*50);  // period for 50 hz
 
 // table of values for 16 samples of one sine wave cycle
 static int sinewave[sampleCount] = { 
-  0x7FF,0xADD,0xDA7,0xF4D,0xFFF,0xF77,0xDA7,0xB40,
-  0x7FF,0x521,0x257,0xB1, 0x0,  0x87, 0x257,0x4BE 
+  0x1FF,0x2B6,0x355,0x3C7,0x3FF,0x3C7,0x355,0x2B6,
+  0x1FF,0x148,0x0A9,0x037,0x000,0x037,0x0A9,0x148 
 };
   
 void setup()
 {
-  //pinMode(outputPin, OUTPUT);   // enable output on the led pin
-  analogWriteResolution(12);    // set the Arduino DAC resolution
-  analogWrite(outputPin, 0);
+  analogWriteResolution(10); // set the Arduino DAC resolution to maximum
 }
 
 void loop()
 {
   int sensorReading = analogRead(sensorPin);    // read the analog input
-  int duration = map(sensorReading, low,high, minDur,maxDur);
-  duration = constrain(duration, minDur,maxDur);    // constrain the value
+  int duration = map(sensorReading, low, high, minDur, maxDur);
+  duration = constrain(duration, minDur, maxDur);
+
   for(int sample=0; sample < sampleCount; sample++) {
     analogWrite(outputPin, sinewave[sample]);
     delayMicroseconds(duration);
