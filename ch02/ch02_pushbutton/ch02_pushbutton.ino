e9092b4b6e16f011411be5a312986175554ffc1b
Brian Jepson <bjepson@gmail.com>
2020-01-07 22:22:28 -0500

    more edits

--- ch02/ch02_pushbutton/ch02_pushbutton.ino
+++ ch02/ch02_pushbutton/ch02_pushbutton.ino
@@ -1,18 +1,20 @@
 /*
-   Pushbutton sketch
-   a switch connected to digital pin 2 lights the LED on pin 13
-*/
+ * Pushbutton sketch
+ * a switch connected to digital pin 2 lights the built-in LED
+ */
 
 const int inputPin = 2;           // choose the input pin (for a pushbutton)
 
-void setup() {
+void setup() 
+{
   pinMode(LED_BUILTIN, OUTPUT);     // declare LED pin as output
   pinMode(inputPin, INPUT_PULLUP);  // declare pushbutton pin as input
 }
 
-void loop(){
+void loop()
+{
   int val = digitalRead(inputPin);  // read input value
-  if (val == LOW)                   // check if the input is LOW
+  if (val == LOW)                   // Input is LOW when the button is pressed
   {
     digitalWrite(LED_BUILTIN, HIGH); // turn LED on if switch is pressed
   }
--- ch02/ch02_pushbutton2/ch02_pushbutton2.ino
+++ ch02/ch02_pushbutton2/ch02_pushbutton2.ino
@@ -1,16 +1,18 @@
 /*
-   Pushbutton sketch
-   a switch connected to pin 2 lights the LED on pin 13
-*/
+ * Pushbutton sketch
+ * a switch connected to pin 2 lights the built-in LED
+ */
 
 const int inputPin = 2;         // choose the input pin (for a pushbutton)
 
-void setup() {
+void setup() 
+{
   pinMode(LED_BUILTIN, OUTPUT);    // declare LED pin as output
   pinMode(inputPin, INPUT_PULLUP); // declare pushbutton pin as input
 }
 
-void loop(){
+void loop()
+{
   int val = digitalRead(inputPin);   // read input value
   if (val == LOW)                    // Input is LOW when the button is pressed
   {
