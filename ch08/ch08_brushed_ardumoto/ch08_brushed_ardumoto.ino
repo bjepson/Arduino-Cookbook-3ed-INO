6c57bc74796a5954483c136002a8968f464f62d7
Brian Jepson <bjepson@gmail.com>
2020-01-20 19:10:09 -0500

    updates

--- ch08/ch08_brushed_ardumoto/ch08_brushed_ardumoto.ino
+++ ch08/ch08_brushed_ardumoto/ch08_brushed_ardumoto.ino
@@ -1,11 +1,11 @@
 /*
- * Brushed_H_Bridge_Direction sketch for motor shield
+ * Brushed_H_Bridge_Direction sketch for Ardumoto shield
  * uses photo sensors to control motor direction
  * robot moves in the direction of a light
- */
+*/
 
-int leftPins[]  = {3,12};   // one pin for PWM, one pin for motor direction
-int rightPins[] = {11,13};
+int leftPins[]  = {3, 2};  // one pin for PWM, one pin for motor direction
+int rightPins[] = {11, 4};
 
 const int MIN_PWM        = 64;  // this can range from 0 to MAX_PWM
 const int MAX_PWM        = 128; // this can range from around 50 to 255
@@ -22,25 +22,25 @@ void setup()
 
 void loop()
 {
-  int leftVal = analogRead(leftSensorPin);
-  int rightVal = analogRead(rightSensorPin);
-  if(sensorThreshold == 0) // have the sensors been calibrated?
-  {  
-     // if not, calibrate sensors to something above the ambient average  
-     sensorThreshold = ((leftVal + rightVal) / 2) + 100 ; 
+  int leftVal = analogRead(leftSensorPin); 
+  int rightVal = analogRead(rightSensorPin); 
+  if (sensorThreshold == 0) // have the sensors been calibrated?
+  {
+    // if not, calibrate sensors to something above the ambient average
+    sensorThreshold = ((leftVal + rightVal) / 2) + 100 ;
   }
-  
-  if( leftVal > sensorThreshold || rightVal > sensorThreshold)
+
+  if ( leftVal > sensorThreshold || rightVal > sensorThreshold)
   {
     // if there is adequate light to move ahead
-    setSpeed(rightPins, map(rightVal,0,1023, MIN_PWM, MAX_PWM));
-    setSpeed(leftPins,  map(leftVal, 0,1023, MIN_PWM, MAX_PWM));
+    setSpeed(rightPins, map(rightVal, 0, 1023, MIN_PWM, MAX_PWM));
+    setSpeed(leftPins,  map(leftVal, 0, 1023, MIN_PWM, MAX_PWM));
   }
 }
 
 void setSpeed(int pins[], int speed )
 {
-  if(speed < 0)
+  if (speed < 0)
   {
     digitalWrite(pins[1], HIGH);
     speed = -speed;
